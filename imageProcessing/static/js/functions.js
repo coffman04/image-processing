be992718a0d99da49980b22e97888c70f1756a3f
coffmanAndrew <coffman.andrew@gmail.com>
2024-10-17 18:16:54 -0400

    updated download page - still having css issues

--- imageProcessing/static/js/functions.js
+++ imageProcessing/static/js/functions.js
@@ -6,10 +6,3 @@ function closeImportDialog(){
     document.getElementById("importDialog").close();
 }
 
-function downloadButton(){
-    document.getElementById("downloadDialog").showModal();
-}
-
-function closeDownloadDialog(){
-    document.getElementById("downloadDialog").close();
-}

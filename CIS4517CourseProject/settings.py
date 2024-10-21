5d86d681f3f1cac0d94242c26ccf0c36be76583a
coffmanAndrew <coffman.andrew@gmail.com>
2024-10-20 20:03:50 -0400

    rds aws setup

--- CIS4517CourseProject/settings.py
+++ CIS4517CourseProject/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'CIS4517CourseProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'imageProcessingDB',
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_ENDPOINT'),
+        'PORT': '5432',
     }
 }
 

1569dfd0dc965490a1b2b88ced9d0895ef7307d0
coffmanAndrew <coffman.andrew@gmail.com>
2024-10-21 10:36:30 -0400

    removed unneccessary comments

--- CIS4517CourseProject/settings.py
+++ CIS4517CourseProject/settings.py
@@ -104,17 +104,6 @@ WSGI_APPLICATION = 'CIS4517CourseProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'imageProcessingDB',
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_ENDPOINT'),
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',

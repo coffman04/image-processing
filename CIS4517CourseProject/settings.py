af56c3b6370a37569a43311c681dd7c55e783ca5
coffmanAndrew <coffman.andrew@gmail.com>
2024-10-21 10:12:22 -0400

    updated to go back to sqlite and make ec2 ip changing easier to handle

--- CIS4517CourseProject/settings.py
+++ CIS4517CourseProject/settings.py
@@ -44,7 +44,7 @@ STORAGES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '18.219.230.219']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', config('AWS_EC2_IP')]
 ROOT_URLCONF = 'CIS4517CourseProject.urls'
 
 # Quick-start development settings - unsuitable for production
@@ -104,14 +104,21 @@ WSGI_APPLICATION = 'CIS4517CourseProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'imageProcessingDB',
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_ENDPOINT'),
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'imageProcessingDB',
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_ENDPOINT'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
